28cd71f605b1e3360aa9e2c979d89f2515dc7e99
OsileJesse123 <jesseosile@gmail.com>
2023-08-11 15:02:04 +0100

    step-18

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:mynotes/extensions/list/filter.dart';
 import 'package:mynotes/services/crud/crud_exceptions.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' show join;
@@ -13,6 +14,8 @@ class NotesService {
   Database? _db;
 
   List<DatabaseNote> _notes = [];
+
+  DatabaseUser? _user;
 
   // All 3 of these is required to create a singleton in
   static final NotesService _shared = NotesService._sharedInstance();
@@ -27,14 +30,30 @@ class NotesService {
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream.filter((note) {
+    final currentUser = _user;
+    if(currentUser != null){
+      return note.userId == currentUser.id;
+    } else {
+      throw UserShouldBeSetBeforeReadingAllNotes();
+    }
+  });
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async{
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async{
     try{
       final user = await getUser(email: email);
+      if(setAsCurrentUser){
+        _user = user;
+      }
       return user;
     } on CouldNotFindUser catch(_){
       final createdUser = await createUser(email: email);
+      if(setAsCurrentUser){
+        _user = createdUser;
+      }
       return createdUser;
     } catch(e){
       rethrow;
@@ -59,8 +78,10 @@ class NotesService {
 
     final updatesCount = await db.update(noteTable, {
       textColumn: text,
-      isSyncedWithCloudColumn: 0
-    });
+      isSyncedWithCloudColumn: 0,
+    }, where: "id = ?",
+      whereArgs: [note.id],
+    );
 
     if(updatesCount == 0){
       throw CouldNotUpdateNote();
